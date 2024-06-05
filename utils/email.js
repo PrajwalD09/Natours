3929d398e26684ef1db31508bd100ca4f9f2d37f
Prajwal Dudalkar <153450849+PrajwalD09@users.noreply.github.com>
2024-06-05 17:55:08 +0530

    Update email.js

--- utils/email.js
+++ utils/email.js
@@ -14,7 +14,7 @@ module.exports = class Email {
   }
 
   newTransport() {
-    if (process.env.NODE_ENV === 'development') {
+    if (process.env.NODE_ENV === 'production') {
       return nodemailer.createTransport({
         service: 'gmail',
         auth: {
