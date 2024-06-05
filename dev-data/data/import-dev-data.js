03cd3b0f6338d2aa8d69f7616e9bd936a2b311a1
Prajwal Dudalkar <153450849+PrajwalD09@users.noreply.github.com>
2024-06-06 01:05:46 +0530

    import dev data

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -13,53 +13,53 @@ dotenv.config({ path: './config.env' }); // will read our variables from file an
 
 // import file data
 
-// const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
 const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
-// const reviews = JSON.parse(
-//   fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
-// );
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
+);
 
 const DB = process.env.DATABASE_ATLAS.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
 );
 
-// mongoose
-//   .connect(DB, {
-//     useNewUrlParser: true,
-//     useCreateIndex: true,
-//     useFindAndModify: false,
-//     useUnifiedTopology: true
-//   })
-//   .then(con => {
-//     console.log('ATLAS DATABASE - Connected successfully');
-//     console.log(con.connections);
-//   });
-
-// LOCAL DATABASE
 mongoose
-  .connect(process.env.DATABASE_LOCAL, {
+  .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true
   })
-  // eslint-disable-next-line no-unused-vars
   .then(con => {
-    console.log('LOCAL DATABASE - Connected successfully');
-    // console.log(con.connections);
+    console.log('ATLAS DATABASE - Connected successfully');
+    console.log(con.connections);
   });
+
+// LOCAL DATABASE
+// mongoose
+//   .connect(process.env.DATABASE_LOCAL, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//     useUnifiedTopology: true
+//   })
+//   // eslint-disable-next-line no-unused-vars
+//   .then(con => {
+//     console.log('LOCAL DATABASE - Connected successfully');
+//     // console.log(con.connections);
+//   });
 
 // IMPORT DATA TO COLLECTION
 
 const importData = async () => {
   try {
-    // await Tour.create(tours);
+    await Tour.create(tours);
     await User.create(users, {
       validateBeforeSave: false
     });
-    // await Review.create(reviews);
-    // console.log('Data successfully imported');
+    await Review.create(reviews);
+    console.log('Data successfully imported');
   } catch (err) {
     // console.log(err);
   }
@@ -70,10 +70,10 @@ const importData = async () => {
 
 const deleteData = async () => {
   try {
-    // await Tour.deleteMany();
+    await Tour.deleteMany();
     await User.deleteMany();
-    // await Review.deleteMany();
-    // console.log('Data successfully deleted');
+    await Review.deleteMany();
+    console.log('Data successfully deleted');
   } catch (err) {
     // console.log(err);
   }
