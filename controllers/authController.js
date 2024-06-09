72300e6701743bf3e05277fce0784bd40aae00be
Prajwal Dudalkar <153450849+PrajwalD09@users.noreply.github.com>
2024-06-09 20:27:58 +0530

    Update authController.js secure and sameSite

--- controllers/authController.js
+++ controllers/authController.js
@@ -19,9 +19,9 @@ const createSendToken = (user, statusCode, res) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
-    samesite: true,
+    sameSite: true,
     httpOnly: true, //cookie will not be modified or accessed by browser
-    secure: true;
+    secure: true
   };
 
   // if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
